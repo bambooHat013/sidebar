2cae7068180cfa75474eb7e8c0198e0fc6efca20
bambooHat013 <watasiwa8531@gmail.com>
2023-08-29 10:58:15 +0800

    加上Header & Footer組件，尚未解決組件之間間距的問題

--- vue-apis6/src/views/Home.vue
+++ vue-apis6/src/views/Home.vue
@@ -1,13 +1,29 @@
 <template>
-  <main class="home-page">
-    <h1>Home</h1>
-    <p>This is the home page</p>
-  </main>
+  <div class="container">
+    <div class="header"><Header /></div>
+    <div class="home-page">
+      <h1>Home</h1>
+      <p>This is the home page</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
-
+import Header from "./Header.vue";
 </script>
 
 <style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+
+.header {
+  display: block;
+  width: 100%;
+}
+
+.home-page {
+  display: block;
+}
 </style>
